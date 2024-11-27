54781f90fad7c4764b91e587a66d648c84bcdb4a
이정현 <cw03125@naver.com>
2024-11-27 20:41:09 +0900

    3차 커밋 네트워킹 로직 싱글톤 패턴 적용

---
+++ SearchGitHubUsers/NetworkManager.h
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.h
+//  SearchGitHubUsers
+//
+//  Created by 이정현 on 11/27/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NetworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SearchGitHubUsers/NetworkManager.m
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.m
+//  SearchGitHubUsers
+//
+//  Created by 이정현 on 11/27/24.
+//
+
+#import "NetworkManager.h"
+
+@implementation NetworkManager
+
+@end
